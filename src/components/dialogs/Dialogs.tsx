f8d0e6f841961843e88d140d7a59af53d0983a32
Anesbek <145551912+Anesbek@users.noreply.github.com>
2024-01-08 20:32:29 +0500

    lesson21 Dialogs.module.css

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import s from  './Dialogs.module.css'
 
 const Dialogs = (props: any) => {
     return (
-        <div >
-            Dialogs
+        <div className={s.dialogs} >
+           <div className={s.dialogsItems}>
+               <div className={s.dialog}>
+                  Anes
+               </div>
+               <div className={s.dialog + ' ' + s.active}>
+                  Ali
+               </div>
+               <div className={s.dialog}>
+                  Ara
+               </div>
+               <div className={s.dialog}>
+                  sAra
+               </div>
+               <div className={s.dialog}>
+                  sArah
+               </div>
+               <div className={s.dialog}>
+                  salah
+               </div>
+           </div>
+            <div className={s.messeges}>
+                <div className={s.messeg}>Hai</div>
+                <div className={s.messeg}>How are you</div>
+                <div className={s.messeg}>Yo</div>
+            </div>
         </div>
     );
 };
